f0d726884a8b930cfd0a12d26546a91f2163cf16
Andre Jacobs <andrejacobs@users.noreply.github.com>
2023-02-20 10:08:13 +0000

    Generating a client certificate as well

--- TLS/generate-self-signed.sh
+++ TLS/generate-self-signed.sh
@@ -5,8 +5,9 @@
 
 # This script will create inside the ./certs directory the following files
 # rootCA.crt: A new root CA cert if it doesn't exist
-# rootCA.key: A signing key for the root CA if the rootCA.crt above was created
-# <DOMAIN_OR_IP>.crt (server certificate) & .key (private key) & .conf (config files used to generate the certs)
+# rootCA.key: A private key for the root CA if the rootCA.crt above was created
+# Inside a sub directory named <DOMAIN_OR_IP>:
+#   <DOMAIN_OR_IP>.crt (server certificate) & .key (private key) along with .conf (config files used to generate the certs)
 
 # NOTES:
 # Depending on what you want to use this for (and the answer better be: only for secured own internal services)
@@ -20,6 +21,7 @@
 # References:
 # https://devopscube.com/create-self-signed-certificates-openssl/
 # https://stackoverflow.com/questions/10175812/how-to-generate-a-self-signed-ssl-certificate-using-openssl
+# https://medium.com/@mertkimyonsen/securing-grpc-connection-with-ssl-tls-certificate-using-go-db3852fe89dd
 
 if [ "$#" -ne 1 ]; then
     >&2 echo "Error: Please provide the domain name or IP of the server."
@@ -46,6 +48,7 @@ CONF_O="Awesome Inc"
 # Organisation unit
 CONF_OU="R&D"
 CONF_CN="${SERVER_DOMAIN}"
+CONF_CLIENT_CN="${SERVER_DOMAIN}.client"
 
 
 # Check if openssl is installed
@@ -59,11 +62,13 @@ pushd . &> /dev/null
 mkdir -p certs
 cd certs
 
+# rootCA ######################################################################
+
+# Create a new root CA and private key if one doesn't exist already
+# NOTE: To inspect the certificate use: openssl x509 -in rootCA.crt -text
 if ! [ -f rootCA.crt ]; then
-    # Create a new root CA and private key
-    # NOTE: To inspect the certificate use: openssl x509 -in rootCA.crt -text
     echo "-----------------------------------------------------------"
-    echo "Creating new rootCA at: certs/rootCA.crt (key: rootCA.key)"
+    echo "Creating new rootCA at: certs/rootCA.crt (private key: rootCA.key)"
     echo "-----------------------------------------------------------"
     echo ""
     openssl req -x509 \
@@ -74,15 +79,6 @@ if ! [ -f rootCA.crt ]; then
                 -subj "/CN=${CONF_CN}/C=${CONF_C}/L=${CONF_L}/ST=${CONF_ST}/O=${CONF_O}/OU=${CONF_OU}" \
                 -keyout rootCA.key -out rootCA.crt
     #           -addext "subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1" \
-
-
-    # Create a new private key for the domain
-    echo ""
-    echo "-----------------------------------------------------------"
-    echo "Creating new private key for the domain at: certs/${SERVER_DOMAIN}.key"
-    echo "-----------------------------------------------------------"
-    echo ""
-    openssl genrsa -out "${SERVER_DOMAIN}.key" ${BITS}
 else
     echo "-----------------------------------------------------------"
     echo "rootCA.crt already exists. No new one will be generated"
@@ -90,10 +86,22 @@ else
     echo ""
 fi
 
+# Server ######################################################################
+mkdir -p "${SERVER_DOMAIN}"
+cd "${SERVER_DOMAIN}"
+
+# Create a new private key for the domain
+echo ""
+echo "-----------------------------------------------------------"
+echo "Creating new private key for the domain at: certs/${SERVER_DOMAIN}/${SERVER_DOMAIN}.key"
+echo "-----------------------------------------------------------"
+echo ""
+openssl genrsa -out "${SERVER_DOMAIN}.key" ${BITS}
+
 # Create config file used for the CSR
 echo ""
 echo "-----------------------------------------------------------"
-echo "Creating new CSR config file at: certs/${SERVER_DOMAIN}.csr.conf"
+echo "Creating new CSR config file at: certs/${SERVER_DOMAIN}/${SERVER_DOMAIN}.csr.conf"
 echo "-----------------------------------------------------------"
 cat > "${SERVER_DOMAIN}.csr.conf" <<EOF
 [ req ]
@@ -124,7 +132,7 @@ EOF
 
 echo ""
 echo "-----------------------------------------------------------"
-echo "Creating new CSR at: certs/${SERVER_DOMAIN}.csr"
+echo "Creating new CSR at: certs/${SERVER_DOMAIN}/${SERVER_DOMAIN}.csr"
 echo "-----------------------------------------------------------"
 # Create a new CSR (Certificate Signing Request)
 openssl req -new \
@@ -134,7 +142,7 @@ openssl req -new \
 
 echo ""
 echo "-----------------------------------------------------------"
-echo "Creating new certificate config at: certs/${SERVER_DOMAIN}.crt.conf"
+echo "Creating new certificate config at: certs/${SERVER_DOMAIN}/${SERVER_DOMAIN}.crt.conf"
 echo "-----------------------------------------------------------"
 # Create config file used to create the server certificate
 cat > ${SERVER_DOMAIN}.crt.conf <<EOF
@@ -151,17 +159,43 @@ EOF
 
 echo ""
 echo "-----------------------------------------------------------"
-echo "Creating new server certificate at: certs/${SERVER_DOMAIN}.crt"
+echo "Creating new Server certificate at: certs/${SERVER_DOMAIN}/${SERVER_DOMAIN}.crt"
 echo "-----------------------------------------------------------"
 # Create the new server certificate
 openssl x509 -req \
              -in "${SERVER_DOMAIN}.csr" \
-             -CA rootCA.crt \
-             -CAkey rootCA.key \
+             -CA ../rootCA.crt \
+             -CAkey ../rootCA.key \
              -CAcreateserial -out "${SERVER_DOMAIN}.crt" \
              -days ${DAYS} \
              -sha512 \
              -extfile "${SERVER_DOMAIN}.crt.conf"
+
+# Client ######################################################################
+echo ""
+echo "-----------------------------------------------------------"
+echo "Creating new Client CSR at: certs/${SERVER_DOMAIN}/client.csr"
+echo "-----------------------------------------------------------"
+# Create a new CSR (Certificate Signing Request) for the client
+openssl req -newkey rsa:${BITS} \
+            -nodes \
+            -keyout "client.key" \
+            -out "client.csr" \
+            -subj "/CN=${CONF_CLIENT_CN}/C=${CONF_C}/L=${CONF_L}/ST=${CONF_ST}/O=${CONF_O}/OU=${CONF_OU}"
+
+echo ""
+echo "-----------------------------------------------------------"
+echo "Creating new Client certificate at: certs/${SERVER_DOMAIN}/client.crt"
+echo "-----------------------------------------------------------"
+# Create the new server certificate
+openssl x509 -req \
+             -in "client.csr" \
+             -CA ../rootCA.crt \
+             -CAkey ../rootCA.key \
+             -CAcreateserial -out "client.crt" \
+             -days ${DAYS} \
+             -sha512 \
+
 
 echo ""
 echo "-----------------------------------------------------------"
